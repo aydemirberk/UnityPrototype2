ea984463ac4cb0d92444ea81d35ab002f5420347
aydemirberk <beerkaydemir@gmail.com>
2022-05-23 22:58:16 +0300

    Uploaded Unity Project
    Uploaded Unity Prototype 2 project to Github

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    public float topbound = 30;
+    public float lowerbound = -10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topbound)
+        {
+            Destroy(gameObject);
+        }
+
+        else if (transform.position.z < lowerbound)
+        {
+            Debug.Log("Game Over");
+            Destroy(gameObject);
+        }
+        
+    }
+}
